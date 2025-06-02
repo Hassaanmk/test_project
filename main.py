46d61289eb4284cd01a8891bc09be5b1569c2b4c
hassaanmk <hassaanmkhan12@gmail.com>
2025-06-03 01:02:56 +0500

    Usernames saved

--- main.py
+++ main.py
@@ -12,14 +12,19 @@ class Blog(BaseModel):
     user_id : int
     query : str
 
-@app.get("/")
+@app.get("/start")
 def read_username():
     print({"Hello": "user"})
 
+@app.put("/username/{username}")
+def put_username(username: str):
+    if username:
+        list_username.append(username)
+    return list_username
 
 @app.post("/users/{user_id}")
 def read_item(bl:Blog):
-    return {"user_id": bl.user_id, "q": bl.query}
+    return {"user_id": bl.user_id, "q": bl.query, "active_users": list_username}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host= "127.0.0.1", port= 8000, reload= True)
